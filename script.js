e2cda6bd37f22cb82bb57583a200008ee32dc7a5
gustavwiese <gusse12345@gmail.com>
2023-04-27 13:51:38 +0200

    commit

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function start() {
   console.log("UwU u made a mistake");
   console.log("Ny linje");
 
+  console.log("Angery");
+  console.log("more angery");
+}
+"use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("UwU u made a mistake");
+  console.log("Ny linje");
+
   console.log("linje igen");
   console.log("linje igen igen");
   console.log("linje igen igen igen");
